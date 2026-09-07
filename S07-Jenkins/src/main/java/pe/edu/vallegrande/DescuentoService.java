b0b7f6b0d9d25d5954c48bb94a1de503ab03e454
ValeryChumpitaz2 <valerychumpitaz2@gmail.com>
2026-09-07 17:00:41 -0500

    JENKINS

---
+++ S07-Jenkins/src/main/java/pe/edu/vallegrande/DescuentoService.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande;
+
+public class DescuentoService {
+
+    public double calcularPrecio(double precio, boolean tieneDescuento) {
+
+        if (tieneDescuento) {
+            return precio * 0.90;
+        }
+
+        return precio;
+    }
+}
